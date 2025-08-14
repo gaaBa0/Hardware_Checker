aa5fe6e8419a4839fe302819a123d37b98de5b96
Gabriel Estevão <130704783+gaaBa0@users.noreply.github.com>
2025-08-14 11:32:38 -0300

    Update main.py
    added comments to the main.py file

--- main.py
+++ main.py
@@ -4,23 +4,26 @@ import wmi
 import ctypes
 import time
 
+# Cria uma variavél importando a biblioteca wmi para checagem do hardware
 c = wmi.WMI()
-vistos = set()
-res = "N"
-commando = "irm https://get.activated.win | iex"
+# Cria uma variável a ser usada na listagem de drivers
+vistos = set() 
+# Cria uma variável com o comando do massgravedev
+commando = "irm https://get.activated.win | iex" 
+# Cria uma variável com comando de powershell para atualizar drivers
 commando2 = 'powershell -Command "Install-Module -Name PSWindowsUpdate -Force; Import-Module PSWindowsUpdate; Get-WindowsUpdate -Install -AcceptAll -IgnoreReboot"'
 
-main = ctk.CTk(fg_color="#1A1A1A")
-main.title("Hardware Checker by Morningstar")
-main.iconbitmap(r"icon.ico")
-main.geometry("900x600")
-main.resizable(False, False)
+main = ctk.CTk(fg_color="#1A1A1A") # Cria a janela principal com cor de fundo
+main.title("Hardware Checker by Morningstar") # Define o título da janela
+main.iconbitmap(r"icon.ico") # Define o ícone da janela
+main.geometry("900x600") # Define o tamanho da janela
+main.resizable(False, False) # Define que o tamanho é estático
 
-main.grid_columnconfigure(0, weight=1)
+main.grid_columnconfigure(0, weight=1) # Configura a coluna 0 para ter peso 1, ela fica responsiva com o que tiver dentro dela
 main.grid_columnconfigure(1, weight=1)
 main.grid_columnconfigure(2, weight=1)
 
-main.grid_rowconfigure(0, weight=1)
+main.grid_rowconfigure(0, weight=1) # Configura a linha 0 para ter peso 1, ela fica responsiva com o que tiver dentro dela
 main.grid_rowconfigure(1, weight=1)
 main.grid_rowconfigure(2, weight=1)
 main.grid_rowconfigure(3, weight=1)
@@ -28,15 +31,17 @@ main.grid_rowconfigure(4, weight=1)
 main.grid_rowconfigure(5, weight=1)
 main.grid_rowconfigure(6, weight=1)
 
-frame1 = ctk.CTkFrame(main, bg_color="transparent", fg_color="transparent")
-frame1.grid(row=1, column=1, sticky="n")
+frame1 = ctk.CTkFrame(main, bg_color="transparent", fg_color="transparent") # Faz o primeiro frame da janela
+frame1.grid(row=1, column=1, sticky="n") # Define ele da linha 1, coluna 1 e com orientação ao norte
 
+# Cria uma label com o título do app, com tamanho fixo e cor de fundo nos padrões
 widget = ctk.CTkLabel(master=frame1, text="Hardware Checker", width=700, fg_color="#ffd700", bg_color="transparent", height=30, text_color="#1A1A1A", corner_radius=20)
 widget.pack()
 
-frame2 = ctk.CTkFrame(main, bg_color="transparent", fg_color="transparent")
+frame2 = ctk.CTkFrame(main, bg_color="transparent", fg_color="transparent") # Cria o segundo frame do app
 frame2.grid(row=3, column=1)
 
+# Faz um textbox para que as mensagens do app sejam mostradas em tela
 text = ctk.CTkTextbox(master=frame2, fg_color="#333333", width=700, height=400, text_color="#F2F2F2", border_color="#000000", bg_color="transparent")
 text.insert(0.0,"""
 Selecione sua opção abaixo:\n\n
@@ -47,32 +52,33 @@ Selecione sua opção abaixo:\n\n
 5. Atualizar drivers
 6. Sair
 """)
-text.configure(state="disabled")
+text.configure(state="disabled") # Configura para que o textbox não seja editável
 text.pack()
 
-frame3 = ctk.CTkFrame(main, bg_color="transparent", fg_color="transparent")
+frame3 = ctk.CTkFrame(main, bg_color="transparent", fg_color="transparent") # Cria o terceiro frame do app para colocar o input e o botão
 frame3.grid(row=5, column=1)
 
 def write(event=None):
-    tempo = time.strftime('%d.%m.%Y.%H_%M_%S')
-    with open(f'drivers_{tempo}.txt', 'w') as file:
-        for driver in c.Win32_PnPSignedDriver():
-            chave = (driver.DeviceName, driver.DriverVersion)
-            if chave not in vistos:
+    tempo = time.strftime('%d.%m.%Y.%H_%M_%S') # Define a data atual com horário em uma variável
+    with open(f'drivers_{tempo}.txt', 'w') as file: # Abre um arquivo e escreve dentro dele os drivers atuais do pc
+        for driver in c.Win32_PnPSignedDriver(): # Seleciona todos os drivers atuais do computador
+            chave = (driver.DeviceName, driver.DriverVersion) # Define a variavel com o driver e a versão
+            if chave not in vistos: # Se a chave não tiver na variavel vistos ele escreve o driver no arquivo
                 file.write(f"{driver.DeviceName} - {driver.DriverVersion}\n")
-                vistos.add(chave)
+                vistos.add(chave) # Adiciona a chave no vistos
         file.close()
+        # Aciona uma janela informando a conclusão do arquivo
         CTkMessagebox(main, fg_color="#000000", bg_color="#000000", text_color="#F2F2F2", title="Arquivo gerado", message="Arquivo gerado com sucesso", button_color="#FFD700", button_hover_color="#FFD966", button_text_color="#1A1A1A")
 
-def opcao(event=None):
-    ans = entry.get()
-    entry.delete(0, "end")
-    text.configure(state="normal")
-    text.delete(0.0, "end")
+def opcao(event=None): # def para escolher a opção do input do usuario
+    ans = entry.get() # define a resposta em uma variavel
+    entry.delete(0, "end") # deleta o que estiver escrito na entry
+    text.configure(state="normal") # muda o textbox para ser editavel
+    text.delete(0.0, "end") # deleta o que estiver no textbox
     try:
-        ans = int(ans)
+        ans = int(ans) # transforma a resposta em inteiro
         match ans:
-            case 1:
+            case 1: # caso a resposta seja 1, os for loop irão pegar cada informação sobre os dipositivos e escrever no textbox
                 text.delete(0.0, "end")
                 for placa in c.Win32_Baseboard():
                     text.insert("end",f"""
@@ -114,27 +120,27 @@ def opcao(event=None):
 └─────────────────────────────────────────────────┘
 """)
                 
-            case 2:
+            case 2: # caso escolha 2 vai escrever em tela todos os drivers que tem e suas versões
                 text.delete(1.0, "end")
                 for driver in c.Win32_PnPSignedDriver():
                     chave = (driver.DeviceName, driver.DriverVersion)
                     if chave not in vistos:
                         text.insert("end",f"{driver.DeviceName} - {driver.DriverVersion}\n")
                         vistos.add(chave)
-            case 3:
+            case 3: # caso escolha 3 vai executar o script como administrador no powershell
                 text.delete(0.0, "end")
                 text.insert("end", "\n...CARREGANDO SCRIPT DE ATIVAÇÃO...\n")
                 ctypes.windll.shell32.ShellExecuteW(
                     None,           
-                    "runas",
+                    "runas", # executa como adm
                     "powershell.exe",
                     commando,
                     None,
                     1
                 )
-            case 4:
+            case 4: # caso escolha 4 vai executar a função write()
                 write()
-            case 5:
+            case 5: # caso escolha 5 vai executar o comando para atualizar drivers no computador
                 ctypes.windll.shell32.ShellExecuteW(
                     None,           
                     "runas",
@@ -143,7 +149,7 @@ def opcao(event=None):
                     None,
                     1
                 )
-            case 6:
+            case 6: # caso escolha 6 vai abrir um pop-up de janela confirmando a saída do usuario
                 msg = CTkMessagebox(main, title="Adeus", option_1="Cancelar", option_3="Sair", fg_color="#000000", bg_color="#000000", text_color="#F2F2F2", title_color="#F2F2F2",message="Obrigado por usar meu script... Até a próxima!", button_color="#FFD700", button_hover_color="#FFD966", button_text_color="#1A1A1A", icon=r"tks.ico")
                 response = msg.get()
                 
@@ -175,11 +181,13 @@ def opcao(event=None):
 """)
     text.configure(state="disabled")
 
+# cria entrada para o input do usuário
 entry = ctk.CTkEntry(frame3, placeholder_text="Selecione uma opção acima...", fg_color="#333333", width=620, height=40, text_color="#F2F2F2", border_color=None,bg_color="transparent", corner_radius=10, border_width=0, placeholder_text_color="#F2F2F2")
-entry.bind("<Return>", command=opcao)
+entry.bind("<Return>", command=opcao) # define que caso o usuario aperte enter vai ser executado a função opcao()
 entry.grid(row=0, padx=5)
 
+# cria o botão para executar a função opcao()
 button = ctk.CTkButton(frame3,75,40,text="Enviar",text_color="#1A1A1A",bg_color="transparent",fg_color="#FFD700", command=opcao, hover_color="#FFD966",border_width=0, corner_radius=10)
 button.grid(row=0, column=2)
 
-main.mainloop()
+main.mainloop() # roda a janela em loop
